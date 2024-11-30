1e869bbe5ec1e71296841621db3d5becd85aaccd
0xtyc <tyc.tingyuchen@gmail.com>
2024-11-30 23:06:04 +0100

    Update UI and controller and rename files

--- src/main/java/com/tyc/collectionintersection/IntersectionApplication.java
+++ src/main/java/com/tyc/collectionintersection/IntersectionApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class IntersectionApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(IntersectionApplication.class.getResource("intersection-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 360, 480);
+        stage.setTitle("Collection Intersection");
         stage.setScene(scene);
         stage.show();
     }
