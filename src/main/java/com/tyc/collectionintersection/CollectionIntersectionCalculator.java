5cf741e83d3d2f063b73a2b062c7df1019b9f65d
0xtyc <tyc.tingyuchen@gmail.com>
2024-11-30 23:05:39 +0100

    Add service and simplify logic

--- src/main/java/com/tyc/collectionintersection/CollectionIntersectionCalculator.java
+++ src/main/java/com/tyc/collectionintersection/CollectionIntersectionCalculator.java
@@ -4,50 +4,28 @@ package com.tyc.collectionintersection;
 import java.util.HashSet;
 
 public class CollectionIntersectionCalculator {
-
-    private int[] smallerCollection;
-    private int[] largerCollection;
-
-    public CollectionIntersectionCalculator(int[] collectionA, int[] collectionB) {
-        this.smallerCollection = collectionA.length < collectionB.length ? collectionA : collectionB;
-        this.largerCollection = collectionA.length < collectionB.length ? collectionB : collectionA;
-    }
-
-    static public int calculateIntersectionSizeWithHash(int[] collectionToHash, int[] collectionToCheck) {
+    static public HashSet<Integer> calculateIntersectionSizeWithHash(int[] collectionToHash, int[] collectionToCheck) {
         HashSet<Integer> collectionSet = new HashSet<>();
         for (int num : collectionToHash) {
             collectionSet.add(num);
         }
-        int intersectionSize = 0;
+        HashSet<Integer> intersectionSet = new HashSet<>();
         for (int num : collectionToCheck) {
-                if (collectionSet.contains(num)) {
-                intersectionSize++;
+            if (collectionSet.contains(num)) {
+                intersectionSet.add(num);
             }
         };
-        return intersectionSize;
+        return intersectionSet;
     }
 
-    private int timeToCalculateIntersectionWithHashing(int runs, int[] collectionToHash, int[] collectionToCheck) {
-        for (int i = 0; i < 3; i++) {
-            // warm up runs
-            calculateIntersectionSizeWithHash(collectionToHash, collectionToCheck);
-        }
-        long totalTime = 0;
+    static public int timeToCalculateIntersectionWithHashing(int[] collectionToHash, int[] collectionToCheck, int runs) {
+        int totalTime = 0;
         for (int i = 0; i < runs; i++) {
             totalTime += ExecutionTimer.timeExecution(() -> {
                 calculateIntersectionSizeWithHash(collectionToHash, collectionToCheck);
                 return null;
             });
         }
-        return (int) (totalTime / runs);
+        return totalTime / runs;
     }
-
-    public int timeToCalculateIntersectionWithHashingSmallerCollection(int runs) {
-        return timeToCalculateIntersectionWithHashing(runs, this.smallerCollection, this.largerCollection);
-    }
-
-    public int timeToCalculateIntersectionWithHashingLargerCollection(int runs) {
-        return timeToCalculateIntersectionWithHashing(runs, this.largerCollection, this.smallerCollection);
-    }
-
 }
